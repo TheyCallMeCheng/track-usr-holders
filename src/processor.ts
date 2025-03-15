f73fc7fa989d39dca3442e6f45acd9b369ae5f21
theycallmecheng <cheng_corp@protonmail.com>
2025-03-15 10:34:31 +0100

    tidy: remove commented code

--- src/processor.ts
+++ src/processor.ts
@@ -10,17 +10,13 @@ import { User } from "./schema/store.js"
 
 const tokenCounter = Counter.register("token")
 const startBlock = 20005922
-// const transfer = Gauge.register("transfer")
-// const transferAcc = Counter.register("transfer_acc")
 const holderAmount = Gauge.register("holderAmount")
-// const holders = Counter.register("holders")
 
 const transferEventHandler = async function (
     event: TransferEvent,
     ctx: SimpleTokenContext
 ) {
     const tokenInfo = await token.getERC20TokenInfo(ctx, ctx.address)
-    // const amount = event.args.value.scaleDown(tokenInfo.decimal)
     const senderAmount = scaleDown(
         await ctx.contract.balanceOf(event.args.from),
         tokenInfo.decimal
@@ -53,5 +49,4 @@ const transferEventHandler = async function (
 
 SimpleTokenProcessor.bind({
     address: RESOLV_PROXY,
-    // startBlock: startBlock,
 }).onEventTransfer(transferEventHandler)
